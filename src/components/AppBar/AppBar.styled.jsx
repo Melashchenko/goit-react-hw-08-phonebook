1fab893a7299e764a77d7e345e00d17d7812e87c
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-03-17 15:21:42 +0200

    Update AppBar.styled.jsx

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -11,7 +11,7 @@ export const Header = styled.header`
   align-items: center;
   justify-content: space-between;
   gap: 12px;
-  padding: 8px 0;
+  padding: 12px 0;
   margin-bottom: 16px;
   border-bottom: 1px solid black;
   > nav {
