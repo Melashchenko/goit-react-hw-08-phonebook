976703eef1d042f8f030c764a3a32e9cd2c7cdc7
Anatolii-melashchenko <toramulo2012@gmail.com>
2023-03-17 16:36:27 +0200

    Added img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import { Box } from 'components/Box';
+import { Link } from 'react-router-dom';
+import PhonebookImg from '../../services/phoneBook.png';
 
 export default function Home() {
   return (
-    <Box as="h1" p={10}>
-      Phonebook
+    <Box
+      as="div"
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      marginBottom={16}
+    >
+      <h1>Phonebook</h1>
+      <Link to="contacts">
+        {' '}
+        <img src={PhonebookImg} alt="PhonebookImg" width={280} height={280} />
+      </Link>
     </Box>
   );
 }
